6286053deacdf9b806d34fa3686446dd5dd69e4e
Ariadna Vigo <arivigodr@gmail.com>
2021-03-01 19:35:35 +0100

    Operations now live in an independent catalog

--- op.c
+++ op.c
@@ -5,10 +5,26 @@
 
 #include "op.h"
 
+#define OP_NAME_SIZE 16
+
+struct op_reg {
+	char id[OP_NAME_SIZE];
+	float (*op_ptr)(float, float);
+};
+
 static float op_add(float a, float b);
 static float op_subst(float a, float b);
 static float op_mult(float a, float b);
 static float op_div(float a, float b);
+
+static struct op_reg op_catalog[] = {
+	{ "+", op_add },
+	{ "-", op_subst },
+	{ "*", op_mult },
+	{ "/", op_div },
+	{ "^", powf },
+	{ "", NULL }
+};
 
 static float
 op_add(float a, float b)
@@ -37,16 +53,12 @@ op_div(float a, float b)
 float 
 (*op(const char *oper))(float, float)
 {
-	if (strcmp(oper, "+") == 0)
-		return op_add;
-	else if (strcmp(oper, "-") == 0)
-		return op_subst;
-	else if (strcmp(oper, "*") == 0)
-		return op_mult;
-	else if (strcmp(oper, "/") == 0)
-		return op_div;
-	else if (strcmp(oper, "^") == 0)
-		return powf; /* From math.h */
-	else
-		return NULL;
+	struct op_reg *ptr;
+
+	for (ptr = op_catalog; strcmp(ptr->id, "") != 0; ++ptr) {
+		if (strcmp(ptr->id, oper) == 0)
+			return ptr->op_ptr;
+	}
+
+	return NULL;
 }
