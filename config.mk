50823c493df2d612fc746ae517b75312b5ee92e5
Ariadna Vigo <arivigodr@gmail.com>
2021-03-20 00:06:26 +0100

    config.mk: reverting accidental -g switch on

--- config.mk
+++ config.mk
@@ -11,8 +11,8 @@ LIBS = -lm
 
 # Flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L
-CFLAGS = -g -std=c99 -Wpedantic -Wall -Wextra ${CPPFLAGS}
-#CFLAGS = -std=c99 -Wpedantic -Wall -Wextra ${CPPFLAGS}
+#CFLAGS = -g -std=c99 -Wpedantic -Wall -Wextra ${CPPFLAGS}
+CFLAGS = -std=c99 -Wpedantic -Wall -Wextra ${CPPFLAGS}
 #LDFLAGS = -static ${LIBS}
 LDFLAGS = ${LIBS}
 
