8ed6b79bdf58a5f3b532874ae9b88606200c34ce
Ariadna Vigo <arivigodr@gmail.com>
2021-07-06 01:09:07 +0200

    sline.c: improved coverage of escape sequences

--- sline.c
+++ sline.c
@@ -15,21 +15,38 @@ enum {
 	VT_DWN,
 	VT_LFT,
 	VT_RGHT,
-	VT_BCKSP,
+	VT_BKSPC,
 	VT_RET
 };
 
 static int term_key(void);
+static int term_esc(char *seq);
+
+static size_t key_bkspc(char *dest, size_t pos);
+static size_t key_default(char *dest, size_t pos, size_t size, char key);
+
+static int
+term_esc(char *seq)
+{
+	if (read(STDIN_FILENO, &seq[0], 1) != 1)
+		return -1;
+	if (read(STDIN_FILENO, &seq[1], 1) != 1)
+		return -1;
+
+	if (seq[0] != '[')
+		return -1;
+
+	if (seq[1] >= '0' && seq[1] <= '9') {
+		if (read(STDIN_FILENO, &seq[2], 1) != 1)
+			return -1;
+	}
+
+	return 0;
+}
 
 static int
 term_key(void)
 {
-	/* 
-	 * We want this to only act upon what we want to. We should discard all 
-	 * we don't plan to implement or haven't implemented yet by returning
-	 * VT_DEF.
-	 */
-
 	char key;
 	char seq[SEQ_SIZE];
 	int nread;
@@ -39,15 +56,9 @@ term_key(void)
 			return -1;
 	}
 
-	/* 
-	 * This isn't fully implemented yet. Some 3-byte sequences are read
-	 * partially still and leak the final '~'.
-	 */
-	if (key == '\x1b') { /* Maybe iscntrl() is better */
-		if (read(STDIN_FILENO, &seq[0], 1) != 1)
-			return key;
-		if (read(STDIN_FILENO, &seq[1], 1) != 1)
-			return key;
+	if (key == '\x1b') {
+		if (term_esc(seq) < 0)
+			return VT_DEF;
 
 		switch (seq[1]) {
 		case 'A':
@@ -62,7 +73,7 @@ term_key(void)
 			return VT_DEF;
 		}
 	} else if (key == '\x7f') {
-		return VT_BCKSP;
+		return VT_BKSPC;
 	} else if (key == '\x0a') {
 		return VT_RET;
 	} else {
@@ -71,12 +82,33 @@ term_key(void)
 	}
 }
 
+static size_t
+key_bkspc(char *dest, size_t pos)
+{
+	if (pos > 0) {
+		--pos;
+		dest[pos] = '\0';
+		write(STDOUT_FILENO, "\b \b", 3);
+	}
+
+	return pos;
+}
+
+static size_t
+key_default(char *dest, size_t pos, size_t size, char key)
+{
+	if (pos < size) {
+		dest[pos] = key;
+		++pos;
+	}
+	write(STDOUT_FILENO, &key, 1);
+
+	return pos;
+}
+
 int
 sline_setup(struct termios *term)
 {
-	/* TODO: Probably wrong */
-
-	/* term->c_iflag &= ~IXON; */
 	term->c_lflag &= ~(ICANON | ECHO);
 	term->c_cc[VMIN] = 0;
 	term->c_cc[VTIME] = 1;
@@ -91,12 +123,12 @@ sline(char *dest, size_t size)
 {
 	/* Work in progress */
 
-	int key;
-	size_t i;
+	char key;
+	size_t pos;
 
 	memset(dest, 0, size);
 
-	i = 0;
+	pos = 0;
 	while ((key = term_key()) != -1) {
 		switch (key) {
 		/* Arrow keys not implemented yet. */
@@ -110,22 +142,14 @@ sline(char *dest, size_t size)
 			continue;
 		case VT_RET:
 			write(STDOUT_FILENO, "\n", 1);
-			return i;
-		case VT_BCKSP:
-			if (i > 0) {
-				--i;
-				dest[i] = '\0';
-				write(STDOUT_FILENO, "\b \b", 3);
-			}
+			return pos;
+		case VT_BKSPC:
+			pos = key_bkspc(dest, pos);
 			break;
 		case VT_DEF:
 			continue;
 		default:
-			if (i < size) {
-				dest[i] = key;
-				++i;
-			}
-			write(STDOUT_FILENO, &key, 1);
+			pos = key_default(dest, pos, size, key);
 			break;
 		}
 
